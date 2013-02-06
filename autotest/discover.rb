e7da56ae48341bc1507490be748584d3bae82378
Rob Westgeest <rob@qwan.it>
2013-02-06 21:07:58 +0100

    removed autotest

--- autotest/discover.rb
+++
@@ -1,8 +0,0 @@
-#
-# (c) Quality Without A Name 2011
-#     Refactoring Course material
-#
-# autotest/discover.rb
-require File.dirname(__FILE__) + '/testunit'
-
-Autotest.add_discovery { "testunit" }
